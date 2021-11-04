9d55f3115e04e530b0e4043eea058ba945b69538
Ishmael-sikhikhi <i.sikhikhi@gmail.com>
2021-11-04 11:52:39 +0200

    testings

--- tables.sql
+++ tables.sql
@@ -10,3 +10,11 @@ insert into fruitbacket (type,qty,price) values ('Banana', 1, 4.50);
 insert into fruitbacket (type,qty,price) values ('Mango', 1, 10.50);
 insert into fruitbacket (type,qty,price) values ('Guava', 1, 2.50);
 insert into fruitbacket (type,qty,price) values ('Grapes', 1, 15.00);
+
+
+create table cars (
+    id serial not null,
+    color text not null,
+    mileage float not null,
+    primary key(id)
+);
